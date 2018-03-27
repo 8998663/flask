0e38def3ab67694bd41a11b8943f8999d6b510b3
8998663 <8998663@qq.com>
2018-03-27 22:32:25 +0800

    初始化配置

---
+++ untitled/flask/restful-5.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2018/3/27 下午8:52
+@Author  : ldx
+@contact : ldx.9@163.com
+@File    : restful-5.py
+@Software: PyCharm
+'''
+
+from flask import Flask
+from flask.ext
+
+if __name__ == '__main__':
